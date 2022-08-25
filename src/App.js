a6bacad9ceacf49000cf935978e5376e7cdae924
Your Name <you@example.com>
2022-08-25 23:16:22 +0900

    Footer Component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Footer from './components/layout/Footer';
 import Navbar from './components/layout/Navbar';
 
 function App() {
@@ -6,6 +7,8 @@ function App() {
     <Router>
       <div className="flex flex-col justify-between-screen">
         <Navbar />
+        <main className='container mx-auto px-3 pb-12'>Content</main>
+        <Footer />
       </div>
     </Router>
   );
