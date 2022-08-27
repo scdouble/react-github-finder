9db2721cbc22eda719e1be1087437571504d7321
Your Name <you@example.com>
2022-08-27 23:25:32 +0900

    Alert Component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Footer from './components/layout/Footer';
+import Alert from './components/layout/Alert';
 import Navbar from './components/layout/Navbar';
 import Home from './pages/Home';
 import About from './pages/About';
@@ -16,6 +17,7 @@ function App() {
           <div className="flex flex-col justify-between-screen">
             <Navbar />
             <main className="container mx-auto px-3 pb-12">
+              <Alert />
               <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/about" element={<About />} />
---
+++ src/components/layout/Alert.jsx
@@ -0,0 +1,19 @@
+import React, { useContext } from 'react';
+import AlertContext from '../../context/alert/AlertContext';
+import alertReducer from '../../context/alert/AlertReducer';
+
+function Alert() {
+  const { alert } = useContext(AlertContext);
+
+  return alert !=null && (
+    <p className="flex items-start mb-4 space-x-2">
+      {alert.type === "error" && (
+        <p className="flex-1 text-base font-semibold leading-7 text-white">
+          <strong>{alert.msg}</strong>
+        </p>
+      )}
+    </p>
+  );
+}
+
+export default Alert;
