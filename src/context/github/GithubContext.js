9bff291d4af8503201c9bf017ed4bab2e86a4441
Your Name <you@example.com>
2022-08-27 23:00:03 +0900

    Search Users

--- src/context/github/GithubContext.js
+++ src/context/github/GithubContext.js
@@ -37,13 +37,35 @@ export const GithubProvider = ({ children }) => {
     });
   };
 
+
+  const searchUsers = async (text) => {
+
+    const params = new URLSearchParams({
+      q : text
+    })
+    setLoading();
+    const response = await fetch(`${GITHUB_URL}/search/users?${params}`, {
+      // headers: { Authorization : `token ${GITHUB_TOKEN}`}
+    });
+    const {items } = await response.json();
+
+    // setUsers(data);
+    // setLoading(false);
+
+    dispatch({
+      type: 'GET_USERS',
+      payload: items,
+    });
+  };
+
+
   const setLoading = () => {
     return dispatch({
       type: 'SET_LOADING',
     });
   };
   return (
-    <GithubContext.Provider value={{ users: state.users, loading: state.loading, fetchUsers }}>
+    <GithubContext.Provider value={{ users: state.users, loading: state.loading, fetchUsers,searchUsers }}>
       {children}
     </GithubContext.Provider>
   );
